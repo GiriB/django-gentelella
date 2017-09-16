de4080a0fdacac4da8a554c1c63902637d95b2e3
Henrique Bastos <henrique@bastos.net>
2017-09-15 22:47:38 -0300

    Single file django demo project

--- gentelella/manage.py
+++ gentelella/manage.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-import os
 import sys
+from django.conf import settings
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gentelella.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=['gentelella'],
+        ROOT_URLCONF='gentelella.urls',
+        SECRET_KEY='SECRET',
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+            }
+        ]
+    )
+
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
