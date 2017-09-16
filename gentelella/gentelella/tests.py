69db4681e37355031c9559b9f642173cdec7a137
Henrique Bastos <henrique@bastos.net>
2017-09-15 22:33:39 -0300

    Simplify genteelly app

--- gentelella/gentelella/tests.py
+++ gentelella/gentelella/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class GentelellaUrlsTest(TestCase):
+    def test_index(self):
+        resp = self.client.get('/gentelella/index.html')
+        self.assertEqual(200, resp.status_code)
+
+    def test_other(self):
+        resp = self.client.get('/gentelella/tables.html')
+        self.assertEqual(200, resp.status_code)
--- gentelella/gentelella/urls.py
+++ gentelella/gentelella/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
-from app import views
+from django.shortcuts import render
+
 
 urlpatterns = [
-    # Matches any html file - to be used for gentella
-    # Avoid using your .html in your resources.
-    # Or create a separate django app.
-    url(r'^.*\.html', views.gentella_html, name='gentella'),
-
-    # The home page
-    url(r'^$', views.index, name='index'),
+    url(r'^(?P<template_name>.*\.html)', render),
+    url(r'^$', render, kwargs=dict(template_name='gentelella/index.html')),
 ]
